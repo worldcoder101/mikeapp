dcee5d63ec6c0af92632f2c3457759e5ca45cb75
worldcoder101 <youngbillionaire101@gmail.com>
2022-11-13 23:24:29 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,70 @@ class MyApp extends StatelessWidget {
       ),
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          title: Text("I AM NOT POOR") ,
-        ),
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Center(
-            child: Image.asset("assets/aaaa.jpeg"),
-          )
-          
-        ),
+            child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const [
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("assets/king.jpeg"),
+            ),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("Micheal miky",style: TextStyle(
+              fontFamily: 'Pacifico',
+              fontSize: 30.0,
+              color: Colors.white,
+            ),),
+            SizedBox(
+              height: 10.0,
+            ),
+            Text("FLUTTER DEVELOPER",style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+            ),),
+            SizedBox(
+              width: 110.0,
+              child: Divider(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                height: 70.0,
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(Icons.phone,color: Colors.teal,),
+                    title: Text("+2349017436465",style: TextStyle(
+                      fontSize: 20.0, color: Colors.teal
+                    ),),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: SizedBox(
+                height: 70.0,
+                child: Card(
+                  child: ListTile(
+                    leading: Icon(Icons.email,color: Colors.teal,),
+                    title: Text("mikky@flutter.com",style: TextStyle(
+                        fontSize: 20.0, color: Colors.teal
+                    ),),
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        )),
       ),
     );
   }
